f6ba78eb423448e8e4271dd2371d9c8c9a630efd
Jhair05 <89157533+Jhair05@users.noreply.github.com>
2024-07-28 20:28:45 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const email = document.querySelector('#email');
     const whats = document.querySelector('#whatsapp');
+    const quiosco = document.querySelector('#quiosco');
 
     email.addEventListener('click', enviarEmail);
     whats.addEventListener('click', enviarWhats);
+    quiosco.addEventListener('click', linkQuiosco);
 
 
     function enviarEmail() {
@@ -13,9 +15,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function enviarWhats() {
         const telefono = '522294913849'; // Número de teléfono con el código de país
-        const mensaje = 'Hola Jhair';
+        const mensaje = 'Hola hermoso, necesito de tu ayuda';
         const url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
         window.open(url, '_blank'); // Abre en una nueva pestaña
+    }
+
+    function linkQuiosco() {
+        const url = 'https://quiosco-la-heladera.vercel.app/order/bebidas';
+        window.open(url, '_blank');
     }
 
 });
